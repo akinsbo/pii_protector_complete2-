015d76e5508fb21a0e984b5722814c8a78b78318
Olaolu Akinsete <akinsbo@gmail.com>
2026-06-27 09:05:09 +0100

    Wire pricing page for configurable Paddle checkout

---
+++ pii_protector_website/payment-config.js
@@ -0,0 +1,20 @@
+window.LEDEBE_PRICING_CONFIG = {
+  provider: "Paddle",
+  checkoutLinks: {
+    pro: {
+      monthly: "mailto:hello@ledebe.com?subject=Pro%20plan%20checkout",
+      annual: "mailto:hello@ledebe.com?subject=Pro%20annual%20plan%20checkout"
+    },
+    team: {
+      monthly: "mailto:hello@ledebe.com?subject=Team%20plan%20checkout",
+      annual: "mailto:hello@ledebe.com?subject=Team%20annual%20plan%20checkout"
+    },
+    enterprise: "mailto:hello@ledebe.com?subject=Enterprise%20enquiry"
+  },
+  paymentMethods: [
+    "Visa",
+    "Mastercard",
+    "American Express",
+    "Bank transfer for enterprise"
+  ]
+};
